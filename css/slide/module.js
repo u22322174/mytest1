7c700ba7e1e067480d1821ffaf56623b2acee343
user <2399121838@qq.com>
2020-06-11 14:41:17 +0800

    测试3

--- css/slide/module.js
+++
@@ -1,7 +0,0 @@
-var fix = document.getElementById('fix'),
-    rgh = 300;
-setInterval(function() {
-    var maxwdh = 1536,
-        wdh = innerWidth;
-        fix.style.right = rgh - (maxwdh - wdh) /2 + 'px'
-},5)
